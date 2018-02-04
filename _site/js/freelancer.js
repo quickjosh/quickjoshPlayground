159c8689a4dd0075c451ca848cc42c5c1a6f921d
quickjosh <quickjosh@github.com>
2018-02-04 13:01:11 -0700

    making nav brand happier

--- _site/js/freelancer.js
+++ _site/js/freelancer.js
@@ -35,3 +35,8 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
